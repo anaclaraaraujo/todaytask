fd7fd0071eca8b0733bb86704dbb37ff26d3fb07
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-11-08 12:59:28 -0300

    feat(TaskItem): add drag and drop

--- src/hook/useTasks.tsx
+++ src/hook/useTasks.tsx
@@ -73,5 +73,6 @@ export function useTasks() {
     addTask,
     removeTask,
     toggleTaskStatus,
+    setTasks
   }
 }
--- src/page/Task/index.tsx
+++ src/page/Task/index.tsx
@@ -7,15 +7,14 @@ import { TaskStatus } from "../../components/TaskStatus";
 import { PlusCircle } from "@phosphor-icons/react";
 import { NoTasks } from "../../components/NoTasks";
 import { useTasks } from "../../hook/useTasks";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 import type { Task } from "../../types/interface";
-
 import styles from './styles.module.css';
-
 
 export function Task() {
   const [inputValue, setInputValue] = useState('');
-  const { tasks, addTask, removeTask, toggleTaskStatus } = useTasks();
+  const { tasks, addTask, removeTask, toggleTaskStatus, setTasks } = useTasks();
 
   const checkedTasksCounter = tasks.reduce((prevValue, currentTask) => {
     if (currentTask.isChecked) {
@@ -23,6 +22,23 @@ export function Task() {
     }
     return prevValue;
   }, 0);
+
+  const handleOnDragEnd = (result: any) => {
+    const { destination, source } = result;
+
+    // Se o item não foi movido para uma nova posição, não faz nada
+    if (!destination) return;
+
+    // Se o item for movido para outra posição
+    if (destination.index !== source.index) {
+      const reorderedTasks = Array.from(tasks);
+      const [removed] = reorderedTasks.splice(source.index, 1);
+      reorderedTasks.splice(destination.index, 0, removed);
+
+      // Atualiza a lista de tarefas com a nova ordem
+      setTasks(reorderedTasks);
+    }
+  };
 
   function handleAddTask() {
     addTask(inputValue);
@@ -51,18 +67,35 @@ export function Task() {
             checkedTasksCounter={checkedTasksCounter}
           />
           {tasks.length > 0 ? (
-            <div>
-              {tasks
-                .sort((a, b) => (a.isChecked === b.isChecked ? 0 : a.isChecked ? 1 : -1))
-                .map((task) => (
-                  <TaskItem
-                    key={task.id}
-                    data={task}
-                    removeTask={removeTask}
-                    toggleTaskStatus={(args) => toggleTaskStatus(args.id, args.value)}
-                  />
-                ))}
-            </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+              <Droppable droppableId="tasksList">
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{ listStyle: 'none', padding: 0 }}
+                  >
+                    {tasks.map((task, index) => (
+                      <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}>
+                            <TaskItem
+                              data={task}
+                              removeTask={removeTask}
+                              toggleTaskStatus={(args) => toggleTaskStatus(args.id, args.value)}
+                            />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
           ) : (
             <NoTasks />
           )}
